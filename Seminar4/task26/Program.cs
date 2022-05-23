3b44b1c20dce8fafb5a9f8d8840390beef6056f7
SteelPlated <mix.singatulin@yandex.ru>
2022-05-24 00:19:32 +0500

    1

---
+++ Seminar4/task26/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number<0) number=number*-1; //для того что отрицательные числа тоже считать
+
+int FindNumberValue(int num)
+{
+    int div = 1;
+    int count = 0;
+    int num2 = 0;
+    while (num > div)
+    {
+        num2 = num / div;
+        div = div * 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(FindNumberValue(number));
+
+/*
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count=0;
+
+while(num!=0)
+{
+    num=num/10;
+    coun++;
+}
+
+Console.WriteLine(count);
+*/
---
+++ Seminar4/task28/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 1;
+// int SummNumber(num)
+//{
+// int summ =1;
+for (int i = 1; i <= num; i++)
+{
+    sum = sum * i;
+}
+//   return summ;
+//}
+
+// int result = SummNumber(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} ={sum}");
